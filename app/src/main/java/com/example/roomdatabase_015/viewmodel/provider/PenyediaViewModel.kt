fec3db055ced3e7597833b3a4d9d8fd3c684f8e9
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 20:36:10 +0700

    pusat kontrol yang bertanggung jawab untuk membuat semua ViewModel

---
+++ app/src/main/java/com/example/roomdatabase_015/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.roomdatabase_015.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase_015.repositori.AplikasiSiswa
+import com.example.roomdatabase_015.viewmodel.EntryViewModel
+import com.example.roomdatabase_015.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
