0dd2d9ea5dfd16746e76a3666eb8b002ff7e3a7f
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 16:16:01 +0700

    implementasi dari fungsi getDatabase() di dalam companion object class DatabaseSiswa

--- app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
@@ -13,3 +13,13 @@ abstract class DatabaseSiswa : RoomDatabase() {
         @Volatile
         private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance)?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context,klass= DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instance=it }
+            }
+        }
+    }
+}
