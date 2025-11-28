b651031358c3655b14282bcacd71d20bd9929bb0
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 16:01:58 +0700

    mendefinisikan sebuah interface bernama ContainerApp

---
+++ app/src/main/java/com/example/roomdatabase_015/repositori/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabase_015.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase_015.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
