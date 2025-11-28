41d7d83d71c4e0740b87153dde06626e8df278d1
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 20:50:26 +0700

    mendefinisikan struktur layar dan cara berpindah antar layar

---
+++ app/src/main/java/com/example/roomdatabase_015/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.roomdatabase_015.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase_015.view.EntrySiswaScreen
+import com.example.roomdatabase_015.view.HomeScreen
+import com.example.roomdatabase_015.view.route.DestinasiEntry
+import com.example.roomdatabase_015.view.route.DestinasiHome
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)}
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
