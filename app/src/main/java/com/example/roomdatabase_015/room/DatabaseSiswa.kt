632db9f48db89a645600842015aecae4b5d58f75
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 16:13:44 +0700

    menambahkan deklarasi fungsi

--- app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
@@ -7,3 +7,5 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao()  : SiswaDao
+
