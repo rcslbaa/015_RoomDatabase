8d3b87f62a7fe4719009e1e70cbe0960cde9dc19
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 20:31:56 +0700

    menambahkan fungsi ekstensi

--- app/src/main/java/com/example/roomdatabase_015/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_015/viewmodel/EntryViewModel.kt
@@ -48,3 +48,11 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya*/
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
