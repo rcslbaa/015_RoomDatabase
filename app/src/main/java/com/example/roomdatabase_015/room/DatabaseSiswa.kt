0b257a37a4576dccdadcaacd34f7bbf29232b523
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 16:12:52 +0700

    mendefinisikan class Database Room

--- app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_015/room/DatabaseSiswa.kt
@@ -1,4 +1,9 @@
 package com.example.roomdatabase_015.room
 
-class DatabaseSiswa {
-}
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
