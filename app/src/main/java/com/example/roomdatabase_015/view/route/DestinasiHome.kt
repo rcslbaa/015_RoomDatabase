09d27b36dd6bc876e6632e18bb69856697f76a72
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 17:16:23 +0700

    mendefinisikan sebuah objek dalam Kotlin bernama DestinasiHome

---
+++ app/src/main/java/com/example/roomdatabase_015/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase_015.view.route
+
+import com.example.roomdatabase_015.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
