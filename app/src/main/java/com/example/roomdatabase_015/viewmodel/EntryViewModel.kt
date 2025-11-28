9f061d03f6da46746262a553554a16a172bbffea
rcslbaa <ifasmarani3125@gmail.com>
2025-11-28 20:30:36 +0700

    mengelola dan merepresentasikan data siswa di lapisan UI

--- app/src/main/java/com/example/roomdatabase_015/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_015/viewmodel/EntryViewModel.kt
@@ -34,3 +34,17 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
         }
     }
 }
+/**
+ * Mewakili Status UI untuk Siswa.
+ */
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
