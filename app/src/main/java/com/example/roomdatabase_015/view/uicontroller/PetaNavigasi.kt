88753ef17492807c553d818c5bea26ba90c0fc00
rcslbaa <ifasmarani3125@gmail.com>
2025-12-05 15:57:10 +0700

    mendaftarkan layar Detail Siswa ke sistem navigasi

--- app/src/main/java/com/example/roomdatabase_015/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase_015/view/uicontroller/PetaNavigasi.kt
@@ -2,7 +2,6 @@ package com.example.roomdatabase_015.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -11,7 +10,12 @@ import com.example.roomdatabase_015.view.HomeScreen
 import com.example.roomdatabase_015.view.route.DestinasiEntry
 import com.example.roomdatabase_015.view.route.DestinasiHome
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.roomdatabase_015.view.DetailSiswaScreen
+import com.example.roomdatabase_015.view.route.DestinasiDetailSiswa
+import com.example.roomdatabase_015.view.route.DestinasiDetailSiswa.itemIdArg
 
 @Composable
 fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier) {
@@ -33,5 +37,6 @@ fun HostNavigasi(
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = { navController.popBackStack() })
         }
-    }
-}
+        //edit 2: tambahkan 2 composable route
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+
