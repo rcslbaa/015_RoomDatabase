4bc0979df6ea569a2371d8e51bafad2cc78c22f7
rcslbaa <ifasmarani3125@gmail.com>
2025-12-05 15:29:19 +0700

    menanadai dan membedakan halaman ini dari halaman lain di sistem navigasi

---
+++ app/src/main/java/com/example/roomdatabase_015/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,6 @@
+package com.example.roomdatabase_015.view.route
+
+import com.example.roomdatabase_015.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
