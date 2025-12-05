246cace37c847aa59afbb16f0e01fe3e862def83
rcslbaa <ifasmarani3125@gmail.com>
2025-12-05 14:49:50 +0700

    menghapus data siswa dari database secara asinkron

--- app/src/main/java/com/example/roomdatabase_015/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase_015/room/SiswaDao.kt
@@ -20,5 +20,8 @@ interface SiswaDao {
     @Query (value = "SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
 
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
 
 }
