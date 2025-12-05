39e2ce3995229ef06e0990c286e0e757df1ce5a1
rcslbaa <ifasmarani3125@gmail.com>
2025-12-05 14:47:59 +0700

    menampilkan data 1 siswa berdasarkan ID secara real-time dari Room Database

--- app/src/main/java/com/example/roomdatabase_015/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase_015/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.roomdatabase_015.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +15,10 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    //Edit : tambah 3 fungsi berikut
+    @Query (value = "SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+
 }
