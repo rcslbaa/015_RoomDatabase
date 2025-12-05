06547b5077066069ff2cb9bc28e8ce2f50d7b425
rcslbaa <ifasmarani3125@gmail.com>
2025-12-05 15:06:05 +0700

    mengupdate data siswa di database

--- app/src/main/java/com/example/roomdatabase_015/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_015/repositori/RepositoriSiswa.kt
@@ -21,3 +21,5 @@ class OfflineRepositoriSiswa(
     //edit 2 : tambah 3 override berikut
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+}
